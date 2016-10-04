823a320b12a93e4e2349dd4c041b7bb0d2f061bf
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 21:53:09 +0200

    piss

--- js/copyclick.js
+++ js/copyclick.js
@@ -2,6 +2,7 @@ var clicker = $('#copy');
 var element = document.getElementById["#copy"]
 
 $('#copy').mousedown(function(){
+	alert("registered click!");
     element.innerHTML = "Copied!";
 	element.style.color = "#ff3300";
 	element.style.borderColor = "#ff3300";
@@ -12,8 +13,9 @@ $('#copy').mousedown(function(){
 $(document).mouseup(function(){
     setTimeout(function() {
 		element.innerHTML = "Copy URL";
-		element.style.color = "";
-		element.style.borderColor = "#92bb35";	
-	}, 3000);
+		element.style.color = "#92bb35";
+		element.style.borderColor = "#92bb35";
+		alert("changed the colors back");
+	}, 1000);
     return false;
 });
