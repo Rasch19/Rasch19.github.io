3a428745dfc703ed6d8225c9e448400855d6a49e
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:31:35 +0200

    pung

--- js/copy.js
+++ js/copy.js
@@ -1,3 +1,17 @@
 $("#copy").click(function() {
 	clipboard.copy(location.href);
+	console.log("niggah");
+    $('#copy').innerHTML = "Copied!";
+	$('#copy').style.color = "#ff3300";
+	$('#copy').style.borderColor = "#ff3300";
+	var check = setTimeout(function() {
+		console.log("got into setTimeout");
+		$('#copy').innerHTML = "Copy URL";
+		$('#copy').style.color = "#92bb35";
+		$('#copy').style.borderColor = "#92bb35";
+		console.log("put the color back");
+	}, 1000);
+	console.log("done with function");
+	
+    return false;
 });
