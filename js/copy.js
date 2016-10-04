8b904d37108ac8f3809770ccf303cb473419dc92
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:48:35 +0200

    kuk

--- js/copy.js
+++ js/copy.js
@@ -7,14 +7,34 @@ $("#copy").click(function() {
     button.innerHTML = "Copied!";
 	button.style.color = "#ff3300";
 	button.style.borderColor = "#ff3300";
-	var check = setTimeout(function() {
+    return false;
+});
+
+$('#copy').mouseup(function () {
+    var check = setTimeout(function() {
 		console.log("got into setTimeout");
+		
+		var button = document.getElementById("copy");
+		
 		button.innerHTML = "Copy URL";
 		button.style.color = "#92bb35";
 		button.style.borderColor = "#92bb35";
 		console.log("put the color back");
-	}, 2500);
+	}, 500);
 	console.log("done with function");
-	
+    return false;
+});
+$('#copy').mouseout(function () {
+    var check = setTimeout(function() {
+		console.log("got into setTimeout");
+		
+		var button = document.getElementById("copy");
+		
+		button.innerHTML = "Copy URL";
+		button.style.color = "#92bb35";
+		button.style.borderColor = "#92bb35";
+		console.log("put the color back");
+	}, 500);
+	console.log("done with function");
     return false;
 });
--- js/copybackup.js
+++ js/copybackup.js
@@ -1,3 +1,20 @@
 $("#copy").click(function() {
-	window.clipboardData.setData("Text",location.href);
+	clipboard.copy(location.href);
+	
+	var button = document.getElementById("copy");
+	
+	console.log("niggah");
+    button.innerHTML = "Copied!";
+	button.style.color = "#ff3300";
+	button.style.borderColor = "#ff3300";
+	var check = setTimeout(function() {
+		console.log("got into setTimeout");
+		button.innerHTML = "Copy URL";
+		button.style.color = "#92bb35";
+		button.style.borderColor = "#92bb35";
+		console.log("put the color back");
+	}, 2500);
+	console.log("done with function");
+	
+    return false;
 });
