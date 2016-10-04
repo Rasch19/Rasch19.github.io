726a41ab79be765e6f85fb5e4fbde14b15a5eeeb
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:35:36 +0200

    pungspark

--- js/copy.js
+++ js/copy.js
@@ -1,14 +1,17 @@
 $("#copy").click(function() {
 	clipboard.copy(location.href);
+	
+	var button = document.getElementById("#copy");
+	
 	console.log("niggah");
-    $('#copy').innerHTML = "Copied!";
-	$('#copy').style.color = "#ff3300";
-	$('#copy').style.borderColor = "#ff3300";
+    button.innerHTML = "Copied!";
+	button.style.color = "#ff3300";
+	button.style.borderColor = "#ff3300";
 	var check = setTimeout(function() {
 		console.log("got into setTimeout");
-		$('#copy').innerHTML = "Copy URL";
-		$('#copy').style.color = "#92bb35";
-		$('#copy').style.borderColor = "#92bb35";
+		button.innerHTML = "Copy URL";
+		button.style.color = "#92bb35";
+		button.style.borderColor = "#92bb35";
 		console.log("put the color back");
 	}, 1000);
 	console.log("done with function");
