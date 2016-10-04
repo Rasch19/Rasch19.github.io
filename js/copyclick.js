bee3353698d81df7537f74587141bb52570a3487
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:19:34 +0200

    piss

--- js/copyclick.js
+++ js/copyclick.js
@@ -1,12 +1,12 @@
 $('#copy').click(function(){
 	console.log("registered click");
-	var elem = document.getElementById["#copy"];
+	var elem = document.getElementById("#copy");
     elem.innerHTML = "Copied!";
 	elem.style.color = "#ff3300";
 	elem.style.borderColor = "#ff3300";
 	var check = setTimeout(function() {
 		console.log("got into setTimeout");
-		var elem = document.getElementById["#copy"];
+		var elem = document.getElementById("#copy");
 		elem.innerHTML = "Copy URL";
 		elem.style.color = "#92bb35";
 		elem.style.borderColor = "#92bb35";
