311109bc4221f2de64949611a40bc4e7cb6a9fe8
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 20:05:42 +0200

    CSS again
    weee

--- js/AddPlayer.js
+++ js/AddPlayer.js
@@ -1,6 +1,14 @@
 
 	$("#Add").click(function() {
+		function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+		}
 		
+		var first = getUrlVars()["url"];
 		var element = document.getElementById("p1");
 		var button = document.getElementById("Add");
 		
@@ -10,8 +18,24 @@
 			{
 				element.innerHTML = (parseInt(element.innerHTML) + 1);
 			}
+			
+			button.innerHTML = "Unready";
+			button.style.borderColor = '#ff3300';
+			button.style.color = '#ff3300';
+			
 		}
-		else {
+		else if(button.innerHTML == "Unready")
+		{
+			if(parseInt(element.innerHTML) < 5)
+			{
+				element.innerHTML = (parseInt(element.innerHTML) + 1);
+			}
+			
+			button.innerHTML = "Ready";
+			button.style.borderColor = '#92bb35';
+			button.style.color = '#92bb35';
+		}
+		else{
 			alert("an Error occured!");
 		}
 		
