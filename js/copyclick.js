97170f0b647f82a87d1a8497a35cbde6b445380d
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:28:50 +0200

    pung

--- js/copyclick.js
+++ js/copyclick.js
@@ -1,13 +1,13 @@
 $('#copy').click(function(){
 	console.log("niggah");
     $('#copy').innerHTML = "Copied!";
-	document.getElementById("#copy").style.color = "#ff3300";
-	document.getElementById("#copy").style.borderColor = "#ff3300";
+	$('#copy').style.color = "#ff3300";
+	$('#copy').style.borderColor = "#ff3300";
 	var check = setTimeout(function() {
 		console.log("got into setTimeout");
-		document.getElementById("#copy").innerHTML = "Copy URL";
-		document.getElementById("#copy").style.color = "#92bb35";
-		document.getElementById("#copy").style.borderColor = "#92bb35";
+		$('#copy').innerHTML = "Copy URL";
+		$('#copy').style.color = "#92bb35";
+		$('#copy').style.borderColor = "#92bb35";
 		console.log("put the color back");
 	}, 1000);
 	console.log("done with function");
