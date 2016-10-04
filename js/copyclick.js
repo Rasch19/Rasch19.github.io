1106779eac245e35f5d61b6beb289ee4410f68f3
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:22:17 +0200

    piss igen

--- js/copyclick.js
+++ js/copyclick.js
@@ -1,15 +1,13 @@
 $('#copy').click(function(){
 	console.log("registered click");
-	var elem = document.getElementById("#copy");
-    elem.innerHTML = "Copied!";
-	elem.style.color = "#ff3300";
-	elem.style.borderColor = "#ff3300";
+    document.getElementById("#copy").innerHTML = "Copied!";
+	document.getElementById("#copy").style.color = "#ff3300";
+	document.getElementById("#copy").style.borderColor = "#ff3300";
 	var check = setTimeout(function() {
 		console.log("got into setTimeout");
-		var elem = document.getElementById("#copy");
-		elem.innerHTML = "Copy URL";
-		elem.style.color = "#92bb35";
-		elem.style.borderColor = "#92bb35";
+		document.getElementById("#copy").innerHTML = "Copy URL";
+		document.getElementById("#copy").style.color = "#92bb35";
+		document.getElementById("#copy").style.borderColor = "#92bb35";
 		console.log("put the color back");
 	}, 1000);
 	console.log("done with function");
