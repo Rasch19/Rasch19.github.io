94cf4e391e462a646a126b9783f852e3aa2fb253
Rasch19 <johannes.s.o.rasch@gmail.com>
2016-10-04 22:43:37 +0200

    I'm dumb
    damn it

--- js/copy.js
+++ js/copy.js
@@ -1,25 +1,20 @@
-
-window.onload = function()
-{
-
-	$("#copybutton").click(function() {
-		clipboard.copy(location.href);
-		
-		var button = document.getElementById("#copybutton");
-		
-		console.log("niggah");
-		button.innerHTML = "Copied!";
-		button.style.color = "#ff3300";
-		button.style.borderColor = "#ff3300";
-		var check = setTimeout(function() {
-			console.log("got into setTimeout");
-			button.innerHTML = "Copy URL";
-			button.style.color = "#92bb35";
-			button.style.borderColor = "#92bb35";
-			console.log("put the color back");
-		}, 1000);
-		console.log("done with function");
-		
-		return false;
-	});
-}
+$("#copy").click(function() {
+	clipboard.copy(location.href);
+	
+	var button = document.getElementById("copy");
+	
+	console.log("niggah");
+    button.innerHTML = "Copied!";
+	button.style.color = "#ff3300";
+	button.style.borderColor = "#ff3300";
+	var check = setTimeout(function() {
+		console.log("got into setTimeout");
+		button.innerHTML = "Copy URL";
+		button.style.color = "#92bb35";
+		button.style.borderColor = "#92bb35";
+		console.log("put the color back");
+	}, 1000);
+	console.log("done with function");
+	
+    return false;
+});
